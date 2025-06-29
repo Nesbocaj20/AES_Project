d60cc014945f56a614dcbd5bbb8c483090702056
Sergey Mashkevich <sergi.mashkevich@gmail.com>
2025-06-29 23:40:04 +0200

    sdsd

--- print(f"\nTampering at byte index {i}:").py
+++
@@ -1,4 +0,0 @@
- print(f"\nTampering at byte index {i}:")
-    print("Modified ciphertext:", errorMessage)
-    print("Computed tag:       ", computedTagError)
-    print("Original tag:       ", recievedTagError)
